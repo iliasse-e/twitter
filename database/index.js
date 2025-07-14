ff32cf23c06f18eb64b17abbeda17920eb77aee9
Iliasse <elabouyi.iliasse@gmail.com>
2025-07-14 13:06:36 +0200

    Connecting to database, Implementing new tweet view

---
+++ database/index.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+exports.clientPromise = mongoose.connect('mongodb+srv://iliasse:JBm3nvc77jPOEh00@cluster0.fpjzuri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then(() => console.log('connexion db ok !'))
+        .catch(err => console.log(err));
