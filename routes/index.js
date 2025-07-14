3846d641b0c0cb87c903e168520e47c73aa329cc
Iliasse <elabouyi.iliasse@gmail.com>
2025-07-14 17:23:12 +0200

    Adding tweets list feature

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const api = require('./api');
+const Tweet = require('../database/models/tweet.model');
 
 router.use('/api', api);
 
@@ -8,7 +9,9 @@ router.get('/tweet/new', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  res.render('tweets/tweet-list')
+  Tweet.find({})
+        .exec()
+        .then( tweets => res.render('tweets/tweet-list', { tweets }) )
 })
 
 module.exports = router;
